aa63e41f3707889e156553f4e5001d43cb1e5104
Archie Martin <98323890+archiemartini@users.noreply.github.com>
2022-11-17 15:01:44 +0000

    run.rb puts steps of HandRanker methods cleanly

--- run.rb
+++ run.rb
@@ -12,20 +12,27 @@ hands_data = JSON.parse(json_from_file)
 ranker = PokerHandRanker.new(json_data: hands_data)
 ranker.generate_hands
 
+puts 'INITIAL HANDS:', ' '
+ranker.hands.map { |hand| puts hand.inspect, ' '}
+
 ranker.set_ranked_hands
-puts 'UNSORTED HANDS', ranker.ranked_hands.map(&:inspect), ' '
+
+puts 'UNSORTED HANDS', ' '
+ranker.ranked_hands.map { |hand| puts hand.inspect, ' '}
 
 ranker.sort_ranked_hands_in_order
 
-puts 'SORTED HANDS', ranker.ranked_hands, ' '
+puts 'SORTED HANDS', ' '
+ranker.ranked_hands.map { |hand| puts hand, ' '}
 
 ranker.extract_original_data
 
-puts 'ORIGINAL DATA', ranker.ranked_hands, ' '
+puts 'REORDERED ORIGINAL DATA'
+ranker.ranked_hands.map{ |hand| puts hand, ' '}
 
-# new_json_data = ranker.ranked_hands
-# options = {
-#   array_nl: "\n ",
-#   indent: " "
-# }
-# File.write('./hands.json', JSON.generate(new_json_data, options))
+new_json_data = ranker.ranked_hands
+options = {
+  array_nl: "\n ",
+  indent: " "
+}
+File.write('./hands.json', JSON.generate(new_json_data, options))

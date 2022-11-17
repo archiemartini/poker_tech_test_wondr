6d4553a0b465dd79e09076f551cdc3d45c3d3888
Archie Martin <98323890+archiemartini@users.noreply.github.com>
2022-11-17 08:31:50 +0000

    tdd'd Card.data attribute

--- lib/card.rb
+++ lib/card.rb
@@ -1,7 +1,9 @@
 class Card
 
+  attr_accessor :data
+
   def initialize(card_data_hash)
-    
+    @data = card_data_hash
   end
 
 end
--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -7,4 +7,8 @@ RSpec.describe 'the Card class:' do
     @card = Card.new(example_card)
   end
 
+  it "holds its initial data in attribute" do
+    expect(@card.data).to include(:suit => 'heart', :value => 5)
+  end
+
 end

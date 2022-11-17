39e988dece41f94ab944aba2840b33bb72c63865
Archie Martin <98323890+archiemartini@users.noreply.github.com>
2022-11-17 09:50:30 +0000

    refactored test data for readability

--- spec/cards_analyzer_spec.rb
+++ spec/cards_analyzer_spec.rb
@@ -4,36 +4,42 @@ RSpec.describe 'the CardsAnalyzer class:' do
 
   subject(:analyzer) { CardsAnalyzer.new }
 
-  @example_card_data
+  before do
+    @example_card_data = {
+      flush: [
+        double(:mock_card, value: 3, suit: "spade"),
+        double(:mock_card, value: 8, suit: "spade"),
+        double(:mock_card, value: 2, suit: "spade"),
+        double(:mock_card, value: 6, suit: "spade"),
+        double(:mock_card, value: 11, suit: "spade"),  
+      ],
+      three_of_a_kind: [
+        double(:mock_card, value: 3, suit: "spade"),
+        double(:mock_card, value: 3, suit: "heart"),
+        double(:mock_card, value: 3, suit: "club"),
+        double(:mock_card, value: 6, suit: "diamond"),
+        double(:mock_card, value: 11, suit: "spade"),  
+      ]
+    }
+  end
 
   describe 'the analyze_cards method' do
 
     describe 'the integration of private function three_of_a_kind?' do
 
-      it "returns the appropriate hash when true" do
-        example_cards_set = [
-          double(:mock_card, value: 3, suit: "spade"),
-          double(:mock_card, value: 3, suit: "heart"),
-          double(:mock_card, value: 3, suit: "club"),
-          double(:mock_card, value: 6, suit: "diamond"),
-          double(:mock_card, value: 11, suit: "spade"),  
-        ]
-        expect(analyzer.analyze_cards(example_cards_set)).to include(:rank => 'Three of a Kind', :strength => 4, :value => 3)
+      it "conditional returns the appropriate hash when true" do
+        example_set = @example_card_data[:three_of_a_kind]
+        expect(analyzer.analyze_cards(example_set)).to include(:rank => 'Three of a Kind', :strength => 4, :value => 3)
       end
     end
 
     describe 'the integration of private function flush?' do
 
-      it "returns the appropriate hash when true" do
-        example_cards_set = [
-          double(:mock_card, value: 3, suit: "spade"),
-          double(:mock_card, value: 8, suit: "spade"),
-          double(:mock_card, value: 2, suit: "spade"),
-          double(:mock_card, value: 6, suit: "spade"),
-          double(:mock_card, value: 11, suit: "spade"),  
-        ]
-        expect(analyzer.analyze_cards(example_cards_set)).to include(:rank => 'Flush', :strength => 6, :value => 11)
+      it "conditional returns the appropriate hash when true" do
+        flush = @example_card_data[:flush] 
+        expect(analyzer.analyze_cards(flush)).to include(:rank => 'Flush', :strength => 6, :value => 11)
       end
+
     end
 
   end
